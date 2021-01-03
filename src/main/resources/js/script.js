7df4b0723be3886f77cb9f170ad0d1d8b24a9d37
Karter Phillips <KarterJP@gmail.com>
2021-01-03 18:02:29 -0500

    Add button operation to nav bar

--- src/main/resources/js/script.js
+++ src/main/resources/js/script.js
@@ -1,1 +1,23 @@
-//script
+var type = null;
+var algo = null;
+
+$(document).ready(function()
+{
+  $("#bubbleSort").click(function(e) {
+    e.preventDefault();
+    doBubbleSort();
+  });
+});
+
+function clearContent ()
+{
+  $("#content").empty();
+}
+
+function doBubbleSort ()
+{
+  clearContent();
+  type = "sorting";
+  algo = "bubbleSort";
+  $("#content").html("<h1 class='heading'>Bubble Sort</h1>");
+}
