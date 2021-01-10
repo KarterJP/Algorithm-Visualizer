6cbf9492ddabed7212d33dc98f1ffe67bc40ed1b
Karter Phillips <KarterJP@gmail.com>
2021-01-10 16:53:16 -0500

    Put algorithms on separate pages

--- src/main/resources/js/script.js
+++ src/main/resources/js/script.js
@@ -15,10 +15,10 @@ const types = {
 }
 
 const speeds = {
-  SLOW: 1000,
-  NORMAL: 500,
-  FAST: 250,
-  SUPERSONIC: 100,
+	SLOW: 1000,
+	NORMAL: 500,
+	FAST: 250,
+	SUPERSONIC: 100,
 }
 
 var speed = speeds.NORMAL;
@@ -27,86 +27,64 @@ var algorithm = algorithms.BUBBLESORT;
 var isRunning = false;
 
 const sleep = (milliseconds) => {
-  return new Promise(resolve => setTimeout(resolve, milliseconds))
+	return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
 $(document).ready(function()
 {
-  $("#navbar-brand").click(function(e) {
-    e.preventDefault();
-  });
-
-  $("#sorting-dropdown").click(function(e) {
-    e.preventDefault();
-  });
+	$("#sorting-dropdown").click(function(e) {
+		e.preventDefault();
+	});
 
 	$("#search-dropdown").click(function(e) {
-    e.preventDefault();
-  });
+		e.preventDefault();
+	});
 
-	$("#linear-search").click(function(e) {
-    e.preventDefault();
-		type = types.SEARCH;
-    algorithm = algorithms.LINEARSEARCH;
-    init();
-  });
-
-  $("#bubble-sort").click(function(e) {
-    e.preventDefault();
-		type = types.SORT;
-    algorithm = algorithms.BUBBLESORT;
-    init();
-  });
-
-  $("#speed").change(function(e) {
-    e.preventDefault();
-    var newSpeed;
-    switch (this.value)
-    {
-      case "slow": newSpeed = speeds.SLOW;
-      break;
-      case "normal": newSpeed = speeds.NORMAL;
-      break;
-      case "fast": newSpeed = speeds.FAST;
-      break;
-      case "supersonic": newSpeed = speeds.SUPERSONIC;
-      break;
-    }
-    speed = newSpeed;
-  });
-});
-
-oCanvas.domReady(function() {
-  init();
+	$("#speed").change(function(e) {
+		e.preventDefault();
+		var newSpeed;
+		switch (this.value)
+		{
+			case "slow": newSpeed = speeds.SLOW;
+			break;
+			case "normal": newSpeed = speeds.NORMAL;
+			break;
+			case "fast": newSpeed = speeds.FAST;
+			break;
+			case "supersonic": newSpeed = speeds.SUPERSONIC;
+			break;
+		}
+		speed = newSpeed;
+	});
 });
 
 function init() {
-  stop();
-  resetProgress();
-  if (canvas != null)
-  {
-    canvas.destroy();
-    objects.length = 0;
-  }
+	stop();
+	resetProgress();
+	if (canvas != null)
+	{
+		canvas.destroy();
+		objects.length = 0;
+	}
 
-  canvas = oCanvas.create({
-    canvas: "#canvas",
-    background: "#EDF5E1",
-    drawEachFrame: true,
-    fps: fps
-  });
+	canvas = oCanvas.create({
+		canvas: "#canvas",
+		background: "#EDF5E1",
+		drawEachFrame: true,
+		fps: fps
+	});
 
-  label = canvas.display.text({
-  	x: 10,
-  	y: 10,
-  	origin: { x: "left", y: "top" },
-  	font: "bold 30px sans-serif",
-  	text: algorithm,
-  	fill: "#333"
-  });
-  canvas.addChild(label);
+	label = canvas.display.text({
+		x: 10,
+		y: 10,
+		origin: { x: "left", y: "top" },
+		font: "bold 30px sans-serif",
+		text: algorithm,
+		fill: "#333"
+	});
+	canvas.addChild(label);
 
-  switch (type)
+	switch (type)
 	{
 		case types.SORT: sortInit();
 		break;
@@ -115,10 +93,41 @@ function init() {
 	}
 }
 
+function start()
+{
+	switch (algorithm)
+	{
+		case algorithms.BUBBLESORT: bubbleSort();
+		break;
+	}
+}
+
+function running()
+{
+	isRunning = true;
+	$("#start").prop("disabled", true);
+}
+
+function stop()
+{
+	iProgress = i;
+	jProgress = j;
+	isRunning = false;
+	$("#start").prop("disabled", false);
+}
+
+function resetProgress()
+{
+	iProgress = 0;
+	jProgress = 0;
+	i = 0;
+	j = 0;
+}
+
 function searchInit()
 {
 	for (var k = 10; k <= canvas.width; k+=80)
-  {
+	{
 		var box = canvas.display.rectangle({
 			x: k,
 			y: (canvas.height/2)-50,
@@ -127,7 +136,7 @@ function searchInit()
 			fill: "rgba(0, 0, 0, 0.1)",
 			stroke: "inside 5px rgba(51, 51, 51, 1)"
 		});
-    canvas.addChild(box);
+		canvas.addChild(box);
 
 		var number = Math.floor(Math.random()*100);
 		var text = canvas.display.text({
@@ -139,109 +148,21 @@ function searchInit()
 			fill: "#333"
 		});
 		canvas.addChild(text);
-    objects.push(number);
-  }
+		objects.push(number);
+	}
 }
 
 function sortInit()
 {
 	for (var k = 50; k <= canvas.width-50; k+=50)
-  {
-    var line = canvas.display.line({
-    	start: { x: k, y: Math.random()*(canvas.height-150)+100 },
-    	end: { x: k, y: canvas.height },
-    	stroke: "25px #333",
-    	cap: "round"
-    });
-    canvas.addChild(line);
-    objects.push(line);
-  }
-}
-
-function start()
-{
-  switch (algorithm)
-  {
-    case algorithms.BUBBLESORT: bubbleSort();
-    break;
-  }
-}
-
-function running()
-{
-  isRunning = true;
-  $("#start").prop("disabled", true);
-}
-
-function stop()
-{
-  iProgress = i;
-  jProgress = j;
-  isRunning = false;
-  $("#start").prop("disabled", false);
-}
-
-function resetProgress()
-{
-  iProgress = 0;
-  jProgress = 0;
-  i = 0;
-  j = 0;
-}
-
-async function bubbleSort ()
-{
-  var n = objects.length;
-
-  if (i >= n-1 && j >= n-i-1)
-  {
-    init();
-  }
-
-  running();
-
-  for (i = iProgress; i < n-1; i++) {
-    for (j = jProgress; j < n-i-1; j++)
-    {
-      objects[j].stroke = "25px #28a745";
-      canvas.redraw();
-      await sleep(speed);
-
-      if (isRunning === false) {
-        return;
-      }
-
-      if (objects[j].length > objects[j+1].length)
-      {
-        moveForward(j);
-      }
-      else
-      {
-        objects[j].stroke = "25px #333";
-        objects[j+1].stroke = "25px #28a745";
-        canvas.redraw();
-      }
-      objects[j].stroke = "25px #333";
-      canvas.redraw();
-    }
-    jProgress = 0;
-    await sleep(speed);
-  }
-  objects[j-1].stroke = "25px #28a745";
-  canvas.redraw();
-  iProgress = 0;
-  stop();
-}
-
-function moveForward (j)
-{
-  objects[j].x += 50;
-
-  objects[j+1].x -= 50;
-
-  var temp = objects[j];
-  objects[j] = objects[j+1];
-  objects[j+1] = temp;
-
-  canvas.redraw();
+	{
+		var line = canvas.display.line({
+			start: { x: k, y: Math.random()*(canvas.height-150)+100 },
+			end: { x: k, y: canvas.height },
+			stroke: "25px #333",
+			cap: "round"
+		});
+		canvas.addChild(line);
+		objects.push(line);
+	}
 }
